959278d3d9f93fc702959b25a844dc99cc7f8394
user-64bit <arth.prajapati@outlook.com>
2024-12-09 14:44:10 +0530

    fix: data not fetching in show-all-forms

--- app/show-all-forms/page.tsx
+++ app/show-all-forms/page.tsx
@@ -1,19 +1,26 @@
+"use client";
+
 import { getAllForms } from "@/actions/getAllForms";
 import FormCard from "../components/form-card/form-card";
+import { useEffect, useState } from "react";
 
-export default async function ShowAllForms() {
-  const allForms = await getAllForms();
-  console.log("data", allForms);
+export default function ShowAllFormsPage() {
+  const [allForms, setAllForms] = useState<any>([]);
+  useEffect(() => {
+    getAllForms().then((forms) => {
+      setAllForms(forms);
+    });
+  }, []);
   return (
     <div className="w-full sm:w-[640px] mx-auto flex flex-col items-center justify-center">
       <div className="mb-8">
-        {allForms.map((form) => {
+        {allForms.map((form: any) => {
           return (
             <FormCard
               key={form.id}
               title={form.title}
-              questions={form.questions.map((question) => question.title)}
-              answers={form.Answer.map((answer) => answer.answer)}
+              questions={form.questions.map((question: any) => question.title)}
+              answers={form.Answer.map((answer: any) => answer.answer)}
             />
           );
         })}
