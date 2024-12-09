2e89d0629e835643d75678645fcdb029cf26d5f5
user-64bit <arth.prajapati@outlook.com>
2024-12-09 11:53:31 +0530

    add: landing page

---
+++ app/components/app-button/app-button.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useState } from "react";
+
+export default function AppButton({
+  title,
+  iconRight,
+  IconLeft,
+  backgroundColor,
+  textColor,
+  borderColor,
+  onClick,
+}: {
+  title: string;
+  iconRight?: string;
+  IconLeft?: React.FC<React.SVGProps<SVGSVGElement>>;
+  backgroundColor?: string;
+  textColor?: string;
+  borderColor?: string;
+  onClick?: () => void;
+}) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    if (onClick) {
+      setIsLoading(true);
+      await onClick();
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div
+      className={`h-[32px] border px-[16px] rounded-[12px] flex items-center`}
+      style={{
+        backgroundColor: backgroundColor,
+        borderColor: borderColor,
+      }}
+      role="button"
+      onClick={handleClick}
+    >
+      {!isLoading ? (
+        <>
+          <div>{IconLeft && <IconLeft />}</div>
+          <div
+            className={`text-[14px] font-semibold`}
+            style={{
+              color: textColor,
+            }}
+          >
+            {title}
+          </div>
+          <div>{iconRight && <img src={iconRight} className="ml-[4px]" />}</div>
+        </>
+      ) : (
+        <div className="">
+          <div className="animate-spin h-4 w-4 border border-t-transparent border-gray-200 rounded-full"></div>
+        </div>
+      )}
+    </div>
+  );
+}
